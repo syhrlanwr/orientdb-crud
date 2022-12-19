4ef1c8e036a926e7ca3b42e9afa11fc2e37aabc9
syarif2002 <rongodsyarif@gmail.com>
2022-12-19 11:15:54 +0700

    add route to edit book page

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ const port = 3000;
 const OrientDB = require('orientjs');
 
 const server = OrientDB({
-    host: 'localhost',
-    port: 2424,
-    username: 'root',
-    password: 'root'
+  host: 'localhost',
+  port: 2424,
+  username: 'root',
+  password: 'root',
 });
 
 const db = server.use({
-    name: 'cobain',
-    username: 'admin',
-    password: 'admin'
+  name: 'cobain',
+  username: 'admin',
+  password: 'admin',
 });
 
 db.exec('CREATE CLASS Book IF NOT EXISTS EXTENDS V');
@@ -25,31 +25,37 @@ db.exec('CREATE PROPERTY Book.year IF NOT EXISTS STRING');
 
 app.set('view engine', 'ejs');
 
-
-app.get('/',async (req, res) => {
-    const sql = 'SELECT FROM Book';
-    const result = await db.query(sql);
-    res.render('index', { books: result });
+app.get('/', async (req, res) => {
+  const sql = 'SELECT FROM Book';
+  const result = await db.query(sql);
+  res.render('index', { books: result });
 });
 
 app.get('/add', (req, res) => {
-    res.render('add-book');
+  res.render('add-book');
 });
 
 app.post('/add', async (req, res) => {
-    const { title, author, year } = req.body;
-    const sql = 'INSERT INTO Book (id, title, author, year) VALUES (:id, :title, :author, :year)';
-    const idquery = 'SELECT MAX(id) FROM Book';
-    const id = await db.query(idquery).then((result) => {
-        if (result[0]['MAX'] == null) {
-            return 1;
-        }
-        return result[0]['MAX'] + 1;
-    });
-    const result = await db.query(sql, { params: { id, title, author, year } });
-    res.redirect('/');
+  const { title, author, year } = req.body;
+  const sql = 'INSERT INTO Book (id, title, author, year) VALUES (:id, :title, :author, :year)';
+  const idquery = 'SELECT MAX(id) FROM Book';
+  const id = await db.query(idquery).then((result) => {
+    if (result[0]['MAX'] == null) {
+      return 1;
+    }
+    return result[0]['MAX'] + 1;
+  });
+  const result = await db.query(sql, { params: { id, title, author, year } });
+  res.redirect('/');
+});
+
+app.get('/edit/:id', async (req, res) => {
+  const { id } = req.params;
+  const sql = 'SELECT FROM Book WHERE id = :id';
+  const result = await db.query(sql, { params: { id } });
+  res.render('edit-book', { book: result[0] });
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
